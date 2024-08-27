ba38e956761f7afc725f9897fa76bc23667ce608
Nileshsaha12 <nileshsaha264@gmail.com>
2024-08-27 08:12:39 +0530

    create variables and left data frame

---
+++ library.py
@@ -0,0 +1,153 @@
+from tkinter import*
+import tkinter as tk 
+from tkinter import ttk
+
+import cx_Oracle
+
+from tkinter import messagebox
+
+import datetime
+
+class LibraryManagementSystem:
+    def __init__(self,root):
+        self.root=root
+        self.root.title("Library Management System")
+        self.root.geometry("1550x800+0+0")
+
+
+        #---------------------------vaiables------------------------------
+
+        self.member_var=StringVar()
+        self.prn_var=StringVar()
+        self.id_var=StringVar()
+        self.firstname_var=StringVar()
+        self.lastname_var=StringVar()
+        self.address_var=StringVar()
+        self.city_var=StringVar()
+        self.postcode_var=StringVar()
+        self.mobile_var=StringVar()
+        self.bookid_var=StringVar()
+        self.booktitle_var=StringVar()
+        self.auther_var=StringVar()
+        self.dateborrowed_var=StringVar()
+        self.datedue_var=StringVar()
+        self.daysonbook_var=StringVar()
+        self.lateratefine_var=StringVar()
+        self.dateoverdue_var=StringVar()
+        self.finalprice_var=StringVar()
+
+
+        lbltitle=Label(self.root,text="LIBRARY MANAGEMENT SYSTEM" ,bg="powder blue",fg="green",bd=20,relief=RIDGE,font=("times new roman",50,"bold"),padx=2,pady=6)
+        lbltitle.pack(side=TOP,fill=X)
+
+        frame=Frame(self.root,bd=12,relief=RIDGE,padx=20,bg="powder blue")
+        frame.place(x=0,y=130,width=1530,height=400)
+
+        #------------------------DataFrameLeft---------------------------
+        DataFrameLeft=LabelFrame(frame,text="Library Membership Information" ,bg="powder blue",fg="black",bd=12,relief=RIDGE,font=("arial",12,"bold"))
+        DataFrameLeft.place(x=0,y=5,width=900,height=350)
+
+        lblMember=Label(DataFrameLeft,bg="powder blue",text="Member Type",font=("times new roman",10,"bold"),padx=2,pady=6)
+        lblMember.grid(row=0,column=0,sticky=W)
+
+        comMember=ttk.Combobox(DataFrameLeft,font=("times new roman",12,"bold"),textvariable=self.member_var,width=27,state="readonly")
+        comMember["value"]=("Admin staff","Student","Lecturer")
+        comMember.grid(row=0,column=1)
+
+        lblPRN_NO=Label(DataFrameLeft,bg="powder blue",text="PRN_No",font=("times new roman",12,"bold"),padx=2)
+        lblPRN_NO.grid(row=1,column=0,sticky=W)
+        txtPRN_NO=Entry(DataFrameLeft,font=("arial",12,"bold"),textvariable=self.prn_var,width=29)
+        txtPRN_NO.grid(row=1,column=1)
+
+        lblId_No=Label(DataFrameLeft,bg="powder blue",text="ID_No",font=("times new roman",12,"bold"),padx=2,pady=4)
+        lblId_No.grid(row=2,column=0,sticky=W)
+        txtId_No=Entry(DataFrameLeft,font=("arial",12,"bold"),textvariable=self.id_var,width=29)
+        txtId_No.grid(row=2,column=1)
+
+        lblFirstName=Label(DataFrameLeft,bg="powder blue",text="FirstName",font=("times new roman",12,"bold"),padx=2,pady=6)
+        lblFirstName.grid(row=3,column=0,sticky=W)
+        txtFirstName=Entry(DataFrameLeft,font=("arial",12,"bold"),textvariable=self.firstname_var,width=29)
+        txtFirstName.grid(row=3,column=1)
+
+        lblLastName=Label(DataFrameLeft,bg="powder blue",text="LastName",font=("times new roman",12,"bold"),padx=2,pady=6)
+        lblLastName.grid(row=4,column=0,sticky=W)
+        txtLastName=Entry(DataFrameLeft,font=("arial",12,"bold"),textvariable=self.lastname_var,width=29)
+        txtLastName.grid(row=4,column=1)
+
+        lblAddress=Label(DataFrameLeft,bg="powder blue",text="Address",font=("times new roman",12,"bold"),padx=2,pady=6)
+        lblAddress.grid(row=5,column=0,sticky=W)
+        txtAddress=Entry(DataFrameLeft,font=("arial",12,"bold"),textvariable=self.address_var,width=29)
+        txtAddress.grid(row=5,column=1)
+
+        lblCity=Label(DataFrameLeft,bg="powder blue",text="City",font=("times new roman",12,"bold"),padx=2,pady=6)
+        lblCity.grid(row=6,column=0,sticky=W)
+        txtCity=Entry(DataFrameLeft,font=("arial",12,"bold"),textvariable=self.city_var,width=29)
+        txtCity.grid(row=6,column=1)
+
+        lblPostCode=Label(DataFrameLeft,bg="powder blue",text="Post Code",font=("times new roman",12,"bold"),padx=2,pady=6)
+        lblPostCode.grid(row=7,column=0,sticky=W)
+        txtPostCode=Entry(DataFrameLeft,font=("arial",12,"bold"),textvariable=self.postcode_var,width=29)
+        txtPostCode.grid(row=7,column=1)
+
+        lblMobile=Label(DataFrameLeft,bg="powder blue",text="Mobile",font=("times new roman",12,"bold"),padx=2,pady=6)
+        lblMobile.grid(row=8,column=0,sticky=W)
+        txtMobile=Entry(DataFrameLeft,font=("arial",12,"bold"),textvariable=self.mobile_var,width=29)
+        txtMobile.grid(row=8,column=1)
+
+        lblBookId=Label(DataFrameLeft,bg="powder blue",text="Book ID:",font=("times new roman",12,"bold"),padx=2,pady=6)
+        lblBookId.grid(row=0,column=2,sticky=W)
+        txtBookId=Entry(DataFrameLeft,font=("arial",12,"bold"),textvariable=self.bookid_var,width=29)
+        txtBookId.grid(row=0,column=3)
+
+        lblBookTitle=Label(DataFrameLeft,bg="powder blue",text="Book Title:",font=("times new roman",12,"bold"),padx=2,pady=6)
+        lblBookTitle.grid(row=1,column=2,sticky=W)
+        txtBookTitle=Entry(DataFrameLeft,font=("arial",12,"bold"),textvariable=self.booktitle_var,width=29)
+        txtBookTitle.grid(row=1,column=3)
+
+        lblAuther=Label(DataFrameLeft,bg="powder blue",text="Author Name:",font=("times new roman",12,"bold"),padx=2,pady=6)
+        lblAuther.grid(row=2,column=2,sticky=W)
+        txtAuther=Entry(DataFrameLeft,font=("arial",12,"bold"),textvariable=self.auther_var,width=29)
+        txtAuther.grid(row=2,column=3)
+
+        lblDateBorrowed=Label(DataFrameLeft,bg="powder blue",text="Date Borrowed:",font=("times new roman",12,"bold"),padx=2,pady=6)
+        lblDateBorrowed.grid(row=3,column=2,sticky=W)
+        txtDateBorrowed=Entry(DataFrameLeft,font=("arial",12,"bold"),textvariable=self.dateborrowed_var,width=29)
+        txtDateBorrowed.grid(row=3,column=3)
+
+        lblDateDue=Label(DataFrameLeft,bg="powder blue",text="Date Due:",font=("times new roman",12,"bold"),padx=2,pady=6)
+        lblDateDue.grid(row=4,column=2,sticky=W)
+        txtDateDue=Entry(DataFrameLeft,font=("arial",12,"bold"),textvariable=self.datedue_var,width=29)
+        txtDateDue.grid(row=4,column=3)
+
+        lblDaysOnBook=Label(DataFrameLeft,bg="powder blue",text="Days on Book:",font=("times new roman",12,"bold"),padx=2,pady=6)
+        lblDaysOnBook.grid(row=5,column=2,sticky=W)
+        txtDaysOnBook=Entry(DataFrameLeft,font=("arial",12,"bold"),textvariable=self.daysonbook_var,width=29)
+        txtDaysOnBook.grid(row=5,column=3)
+
+        lblLateReturnFine=Label(DataFrameLeft,bg="powder blue",text="Late Return Fine:",font=("times new roman",12,"bold"),padx=2,pady=6)
+        lblLateReturnFine.grid(row=6,column=2,sticky=W)
+        txtLateReturnFine=Entry(DataFrameLeft,font=("arial",12,"bold"),textvariable=self.lateratefine_var,width=29)
+        txtLateReturnFine.grid(row=6,column=3)
+
+        lblDateOverdate=Label(DataFrameLeft,bg="powder blue",text="Date Over Due:",font=("times new roman",12,"bold"),padx=2,pady=6)
+        lblDateOverdate.grid(row=7,column=2,sticky=W)
+        txtDateOverdate=Entry(DataFrameLeft,font=("arial",12,"bold"),textvariable=self.dateoverdue_var,width=29)
+        txtDateOverdate.grid(row=7,column=3)
+
+        lblActualPrice=Label(DataFrameLeft,bg="powder blue",text="Actual Price",font=("times new roman",12,"bold"),padx=2,pady=6)
+        lblActualPrice.grid(row=8,column=2,sticky=W)
+        txtActualPrice=Entry(DataFrameLeft,font=("arial",12,"bold"),textvariable=self.finalprice_var,width=29)
+        txtActualPrice.grid(row=8,column=3)
+
+       
+
+        #----------------------------DataFrameRight----------------------------------
+
+
+
+
+if __name__ == "__main__":
+    root=Tk()
+    obj=LibraryManagementSystem(root)
+    root.mainloop()
+
